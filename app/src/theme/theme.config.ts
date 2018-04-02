b2371d45819856ea5231dd192f86e4a066ba684e
unknown <447641028@qq.com>
2018-04-03 06:53:48 +0800

    x3

--- app/src/theme/theme.config.ts
+++ app/src/theme/theme.config.ts
@@ -1,7 +1,7 @@
 // export const BASEURL = 'http://sapi.bainid.com';
 // export const BASEURL = 'http://sadmin.bainid.com';
-export const BASEURL = 'http://101.132.70.102/api/index.php';
-export const BASEURLIMG = 'http://101.132.70.102';
+export const BASEURL = 'http://www.robinjy.com/api/index.php';
+export const BASEURLIMG = 'http://www.robinjy.com';
 
 //export const BASEURL = 'http://api.skullfang.online';
 // export const BASEURL = 'http://www.api.skullfang.cn';
